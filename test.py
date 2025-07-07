2164650afc28ed086148b4aff3777b4e56161f28
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-07 21:58:16 +0300

    Added 'Print-side' of code

---
+++ test.py
@@ -0,0 +1,8 @@
+import random
+
+def get_difficulty():
+    print("Welcome to the Number Guessing Game!")
+    print("Choose Difficulty Level:")
+    print("1. Easy (10 guesses)")
+    print("2. Medium (7 guesses)")
+    print("3. Hard (5 guesses)")
